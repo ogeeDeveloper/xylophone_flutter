190b9ca3408a725e9d31b2f596c00960068c6b46
ogeeDeveloper <omarogrant202@hotmail.com>
2024-02-19 20:43:42 -0500

    installed and setup the audio player flutter package in the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-          child: Container(),
+        body: Player(),
+      ),
+    );
+  }
+}
+
+class Player extends StatefulWidget {
+  const Player({super.key});
+
+  @override
+  State<Player> createState() => _PlayerState();
+}
+
+class _PlayerState extends State<Player> {
+  final player = AudioPlayer();
+
+  @override
+  void initState() {
+    super.initState();
+    initPlayer();
+  }
+
+  Future<void> initPlayer() async {
+    // Any additional player setup can go here.
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Center(
+        child: TextButton(
+          onPressed: () async => {
+            // await player.setSource(AssetSource('note1.wav')),
+            player.play(AssetSource('note1.wav'))
+          },
+          child: const Text('Click me'),
         ),
       ),
     );
